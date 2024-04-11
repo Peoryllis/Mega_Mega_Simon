6c86e69da089ae29c16f08248c57091d45df4f37
Anaya Chinyere <136634937+Peoryllis@users.noreply.github.com>
2024-04-10 18:00:33 -0700

    finished project!!!

--- main_game.py
+++ main_game.py
@@ -40,6 +40,9 @@ class MegaSimon(Frame):
 
         self.title.place(relx=0.5, rely=0, relwidth=0.5, relheight=0.1, anchor='n')
 
+        self.score = 0
+        self.highscore = 0
+
         self.scoreLabel = Label(
             self,
             text='Score: 0',
@@ -64,6 +67,8 @@ class MegaSimon(Frame):
 
         self.highScoreLabel.place(relx=0, rely=0.05, relwidth=0.25, relheight=0.05)
 
+        self.master.update()
+
         self.computer_sequence = []
         self.player_sequence = []
 
@@ -82,7 +87,16 @@ class MegaSimon(Frame):
 
     #make function to set up matrix
 
-        def begin(oldFrame, size):
+        self.title.configure(
+            text='Simon!!'
+        )
+
+        def begin(size, oldFrame):
+
+            oldFrame.destroy()
+
+            self.master.update()
+
             size = list(item for item in size if item.isnumeric())
             size = size[:len(size)//2]
 
@@ -95,16 +109,15 @@ class MegaSimon(Frame):
 
             self.matrix = Square_Matrix(self, matrixSize)
             self.matrix.place(relx=0, rely=0.15, relwidth=1, relheight=0.85)
+            self.matrix.bind_all('<Button>', lambda e: self.compare_sequences(), '+')
 
-            oldFrame.destroy()
-
-            root.update()
+            self.master.update()
 
             self.computer_sequence = []
             self.player_sequence = []
 
             self.playerTurn = False
-            self.playerWinning = None
+            self.playerWinning = True
 
             self.play_game()
 
@@ -132,14 +145,14 @@ class MegaSimon(Frame):
         setting = StringVar()
         setting.set(choices[0])
 
-        drop = OptionMenu(self, setting, *choices)
+        drop = OptionMenu(temporaryFrame, setting, *choices)
         self.master.update()
         
         drop.place(relx=0.5, rely=0.25, relwidth=0.5, anchor='center')
 
         beginButton = tk2.LabelButton(
-            self,
-            command=lambda: (begin(temporaryFrame, setting.get()), temporaryFrame.destroy()),
+            temporaryFrame,
+            command=lambda: (begin(setting.get(), temporaryFrame)),
             kwargs={
                 'bg':'white',
                 'fg': 'black',
@@ -150,12 +163,23 @@ class MegaSimon(Frame):
         )
 
         beginButton.place(relx=0.5, rely=0.5, relwidth=0.5, relheight=0.1, anchor='center')
+        self.master.update()
 
     def computer_turn(self):
         '''
         computer_turn
         has the computer take their turn
         '''
+
+
+        self.title.configure(
+            text='Computer Turn',
+            font=self.font + ['40']
+        )
+
+        root.update()
+        time.sleep(1)
+
         for location in self.computer_sequence:
             self.matrix.button_pressed(location)
             time.sleep(1)
@@ -165,16 +189,110 @@ class MegaSimon(Frame):
         )
 
         self.matrix.button_pressed(self.computer_sequence[-1])
+        self.master.update()
 
         
 
     def compare_sequences(self):
-        pass
-    def play_game(self):
-        pass
-    def game_over(self):
-        pass
+        '''
+        compare_sequences()
+        make sure the user did not make any mistakes
+        '''
 
+
+        if self.playerTurn:
+            self.player_sequence.append(self.matrix.lastButton)
+
+            if self.computer_sequence[:len(self.player_sequence)] != self.player_sequence: 
+                self.playerWinning = False
+
+    def play_game(self):
+        '''
+        play_game()
+        plays a game of mega simon
+        '''
+        while self.playerWinning:
+            if len(self.player_sequence) == len(self.computer_sequence):
+                self.playerTurn = False
+                self.score = len(self.player_sequence)
+                if self.highscore < self.score: self.highscore = self.score
+
+                self.highScoreLabel['text'] = f'High score: {self.highscore}'
+                self.scoreLabel['text'] = f'Score: {self.score}'
+                self.master.update()
+
+                self.player_sequence = []
+                self.computer_turn()
+                self.master.update()
+            else:
+                self.playerTurn = True
+                self.master.update()
+
+
+                self.title.configure(
+                    text='Your turn',
+                )
+
+                self.master.update()
+
+        self.game_over()
+        self.player_sequence = []
+
+    def game_over(self):
+        '''
+        make game_over frame
+        '''
+
+        self.matrix.unbind_all(['<Button>'])
+        self.matrix.destroy()
+        
+
+        self.master.update()
+        self.title.configure(
+            text='GAME OVER'
+        )
+    
+        temporaryFrame = Frame(
+            self,
+            bg=self['bg']
+        )
+        temporaryFrame.pack(fill='both', expand=1)
+
+        instructionLabel = Label(
+            temporaryFrame,
+            text='Play again or quit the game?',
+            bg='white',
+            fg='black',
+            font=self.font + [20]
+        )
+        instructionLabel.place(relx=0.5, rely=0, relwidth=0.5, relheight=0.1, anchor='n')
+
+        restartButton = tk2.LabelButton(
+            temporaryFrame,
+            command= lambda: (self.setup(), temporaryFrame.destroy()),
+            kwargs={
+                'bg': 'white',
+                'fg': 'black',
+                'text': 'Play again',
+                'font': self.font + [18]
+            }
+        )
+
+        restartButton.place(relx=0.25, rely=0.25, relwidth=0.25, relheight=0.1, anchor='center')
+
+        endButton = tk2.LabelButton(
+            temporaryFrame,
+            command= lambda: (root.destroy()),
+            kwargs={
+                'bg': 'white',
+                'fg': 'black',
+                'text': 'end game',
+                'font': self.font + [18]
+            }
+        )
+
+        endButton.place(relx=0.75, rely=0.25, relwidth=0.25, relheight=0.1, anchor='center')
+        
 
 Simon = MegaSimon(root)
 
